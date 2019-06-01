997ed8a921d455d651e05cd87f17732d6c7d4095
Jigar Mehta Git <jigar.digital@gmail.com>
2019-06-02 03:10:59 +0530

    modified Sample1.java

--- src/pack1/Sample1.java
+++ src/pack1/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World Sample1");
 		System.out.println("Hello World Sample1");
+		System.out.println("I'm learning jenkins and git integration");
 		
 			
 	}
