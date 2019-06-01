8a7e59292c0022c54d702239b09605dd0d0e7a3a
Jigar Mehta Git <jigar.digital@gmail.com>
2019-06-02 02:06:20 +0530

    First commit

---
+++ src/pack1/Sample1.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Sample1");
+		System.out.println("Hello World Sample1");
+		
+			
+	}
+
+}
