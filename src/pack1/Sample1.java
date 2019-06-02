99ef2eeeb7271036083eaf92e93143bab1ed023b
Jigar Mehta Git <jigar.digital@gmail.com>
2019-06-02 13:09:50 +0530

    some changes

--- src/pack1/Sample1.java
+++ src/pack1/Sample1.java
@@ -3,11 +3,7 @@ package pack1;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Sample1");
-		System.out.println("Hello World Sample1");
-		System.out.println("I'm learning jenkins and git integration");
-		
-			
+		System.out.println("Sample1 - Hello World Sample1");
 	}
 
 }
--- src/pack1/Sample2.java
+++ src/pack1/Sample2.java
@@ -3,7 +3,7 @@ package pack1;
 public class Sample2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World Sample2");
+		System.out.println("Sample2 - Hello World Sample2");
 	}
 
 }
