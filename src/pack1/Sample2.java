bc02c0f9b4e12e4527e8b78ca8b3c16afc246e7c
Jigar Mehta Git <jigar.digital@gmail.com>
2019-06-02 13:22:42 +0530

    made changes

--- src/pack1/Sample2.java
+++ src/pack1/Sample2.java
@@ -4,6 +4,7 @@ public class Sample2 {
 
 	public static void main(String[] args) {
 		System.out.println("Sample2 - Hello World Sample2");
+		System.out.println("Sample2 - Hello World Sample2");
 	}
 
 }
